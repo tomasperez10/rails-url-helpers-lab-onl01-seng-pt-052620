7d835187f612c9e2856e2b50451a0078eaafde45
Tomas Perez <tomasitopg6@gmail.com>
2020-09-28 16:38:24 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,7 +8,7 @@ class StudentsController < ApplicationController
   def show
   end
 
-  def activate_student_path(@student)
+  def activate_student_path
     @student = Student.find(params[:id])
     if @student.active
       @student.active = false
